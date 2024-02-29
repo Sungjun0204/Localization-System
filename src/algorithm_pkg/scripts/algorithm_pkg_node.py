e85fa495332b9069cdef12466f9b3ce263f25363
Sungjun Kim <kxt1234.max@gmail.com>
2024-02-29 18:12:38 +0900

    2024.02.29 updated

--- src/algorithm_pkg/scripts/algorithm_pkg_node.py
+++ src/algorithm_pkg/scripts/algorithm_pkg_node.py
@@ -16,8 +16,7 @@ import pprint
 # 전역변수 선언
 zero_setting_flag = 0           # 4~5번 뒤에 offset을 작동시키기 위한 flag변수 선언
 #first_value = np.array([0.118, -0.118, 0.065]) # 위치 초기 값
-#first_m = np.array([0.30, 0.30, 0.30])         # 자계강도 초기값
-first_value = [0.118, -0.118, 0.065, 5023, 5023, 0]
+first_value = [0.118, -0.118, 0.065, 800, 800, 0]
 full_packet = ""                # 패킷 값을 저장하는 리스트 변수
 sensor_data = []                # 해체작업을 진행할 패킷 값을 저장하는 리스트 변수
 packet_count = 0                       # 분할되어 들어오는 패킷을 총 10번만 받게 하기 위해 카운트를 세는 변수
@@ -179,9 +178,12 @@ def offset_Setting():
     # 먼저 H를 정규화시켜야 함
     if(flag == 0):        # 알고리즘 첫 시작에만 정규화시키면 됨. 이 후로는 알아서 정규화 값을 기준으로 값을 추정할 것임
         initial_guess[5] = ((-1)*(initial_guess[3]*initial_guess[4]) / (initial_guess[3]+initial_guess[4]))  # Z값을 조건에 맞게 계산 후 대입 (m^2 + n^2 + p^2 = 1)
-        H_norm = np.linalg.norm(initial_guess[3:6])
-        initial_guess[3:6] = np.array(initial_guess[3:6]) / H_norm
         flag = 1
+    elif(flag == 1):
+        initial_guess[5] = (1-(initial_guess[3]**2)-(initial_guess[4]**2))
+        H_norm = np.linalg.norm(initial_guess[3:6])                    # 정규화 계산을 위한 norm값 계산
+        initial_guess[3:6] = np.array(initial_guess[3:6]) / H_norm     # 초기 자계강도(H) 벡터에 대한 정규화 진행
+
 
     result_pos = least_squares(residuals, initial_guess, method='lm')    # Levenberg-Marquardt Algorithm 계산
     
@@ -201,7 +203,7 @@ def offset_Setting():
 # 측정한 자기장 값과 계산한 자기장 값 사이의 차이를 계산하는 함수
 # 여기서 오차 제곱까지 해 줄 필요는 없음. least_squares에서 알아서 계산해 줌
 def residuals(init_pos):
-    global array_Val, P, first_value
+    global array_Val, P
     differences = [0,0,0,0,0,0] # 센서 값과 계산 값 사이의 잔차 값을 저장하는 배열변수 초기화
 
     # 위치에 대한 잔차 값의 총합 저장
@@ -210,7 +212,9 @@ def residuals(init_pos):
         differences[0] += buffer_residual[0]    # 각 센서들의 잔차를 각 축 성분끼리 더한다
         differences[1] += buffer_residual[1]
         differences[2] += buffer_residual[2]
-    differences[3:6] = first_value[3:6]
+        
+        normalized_B = array_Val[i] / np.linalg.norm(array_Val[i])   # 측정한 자기장 값을 정규화
+        differences[3:6] += normalized_B - np.array(cal_B(init_pos, P[i]))[3:6]     # 정규화된 측정 자기장 값과 예상 자기장 값 사이의 차이 계산
 
     #pprint.pprint(differences) # 계산한 잔차 값의 총합 출력
     return differences
@@ -221,19 +225,17 @@ def residuals(init_pos):
 # 자석의 자기밀도를 계산하는 함수 
 # A: 자석의 현재 위치좌표, P: 센서의 위치좌표, H: 자석의 자계강도
 def cal_B(A_and_H, P):
-    global MU, M_T # first_m
+    global MU, M_T 
     A = [A_and_H[0], A_and_H[1], A_and_H[2]]    # 위치 값 따로 A 리스트에 저장
     H = [A_and_H[3], A_and_H[4], A_and_H[5]]    # 자계강도 값 따로 H 리스트에 저장    
-
-    # H = first_m
-
+    
     N_t = ((MU / MU0) * MU0 * M_T) / (4*(np.pi))    # 상수항 계산
     
     # A[0~2]: A, A[3~5]: H를 의미함
     b_x = N_t * (((3*h_dot_p(A, H, P))*(P[0]-A[0]) / (distance_3d(P,A) ** 5)) - ((H[0]) / (distance_3d(P,A) ** 3)))
     b_y = N_t * (((3*h_dot_p(A, H, P))*(P[1]-A[1]) / (distance_3d(P,A) ** 5)) - ((H[1]) / (distance_3d(P,A) ** 3))) 
     b_z = N_t * (((3*h_dot_p(A, H, P))*(P[2]-A[2]) / (distance_3d(P,A) ** 5)) - ((H[2]) / (distance_3d(P,A) ** 3)))
-
+    
     #print([b_x, b_y, b_z])
     return [b_x, b_y, b_z, H[0], H[1], H[2]]            # 최종 자기밀도 값 반환
 
